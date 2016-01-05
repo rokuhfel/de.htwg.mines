880d0fe194664b4df62e6ad85a01d6933382af2e
Sascha Diefenthäler <sascha.diefenthaeler@gmail.com>
2016-01-05 11:56:00 +0100

    Initial Web UI

--- app/controllers/MainController.scala
+++ app/controllers/MainController.scala
@@ -5,8 +5,5 @@ import model.impl.Grid
   * Created by Ron on 17.12.2015.
   */
 class MainController {
-  def main(args: Array[String]){
-    val grid = new Grid(1,1,1)
-    grid.toString()
-  }
+    val grid = new Grid(4,4,3)
 }
